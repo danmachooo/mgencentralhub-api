85814d73bb1de344cc11fe1d9d840b1fa6f6ce3f
danmachooo <johnpauldanmachi@gmail.com>
2026-03-05 10:46:25 +0800

    Added optional to name

--- src/schema/departments/update-department.schema.ts
+++ src/schema/departments/update-department.schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 
 export const updateDepartmentSchema = z.object({
-	name: z.string().min(1),
+	name: z.string().min(1).optional(),
 	description: z.string().min(1).optional(),
 })

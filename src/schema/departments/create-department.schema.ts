29c45efbac7b45db5de65afc33370d7e9514c615
danmachooo <johnpauldanmachi@gmail.com>
2026-03-05 12:23:01 +0800

    Added strict()

--- src/schema/departments/create-department.schema.ts
+++ src/schema/departments/create-department.schema.ts
@@ -3,4 +3,4 @@ import { z } from "zod"
 export const createDepartmentSchema = z.object({
 	name: z.string().min(1),
 	description: z.string().min(1).optional(),
-})
+}).strict()
--- src/schema/departments/update-department.schema.ts
+++ src/schema/departments/update-department.schema.ts
@@ -3,4 +3,4 @@ import { z } from "zod"
 export const updateDepartmentSchema = z.object({
 	name: z.string().min(1).optional(),
 	description: z.string().min(1).optional(),
-})
+}).strict()

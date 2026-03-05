ee5ea1de5c359f0ca222c46506cab2b3dfc9ccfd
danmachooo <johnpauldanmachi@gmail.com>
2026-03-05 16:10:54 +0800

    Adde name in update user profile schema

--- src/schema/users/profile/update-user-profile.schema.ts
+++ src/schema/users/profile/update-user-profile.schema.ts
@@ -1,3 +1,6 @@
 import { createUserProfileSchema } from "@/schema/users/profile/create-user-profile.schema"
+import z from "zod"
 
-export const updateUserProfileSchema = createUserProfileSchema.partial()
+export const updateUserProfileSchema = createUserProfileSchema.partial().extend({
+	name: z.string().min(1).max(30),
+})

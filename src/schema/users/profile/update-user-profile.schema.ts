d874ba7a8c74571ef0cc14d6fab1b48fc5bc4ab5
danmachooo <johnpauldanmachi@gmail.com>
2026-03-11 17:18:09 +0800

    Updated the userprofile schema

--- src/schema/users/profile/update-user-profile.schema.ts
+++ src/schema/users/profile/update-user-profile.schema.ts
@@ -1,6 +1,12 @@
 import { createUserProfileSchema } from "@/schema/users/profile/create-user-profile.schema"
 import z from "zod"
 
-export const updateUserProfileSchema = createUserProfileSchema.partial().extend({
-	name: z.string().min(1).max(30),
-})
+export const updateUserProfileSchema = createUserProfileSchema
+	.partial()
+	.omit({
+		roleId: true
+	})
+	.extend({
+		name: z.string().min(1).max(30)
+	})
+
